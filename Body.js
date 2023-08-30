513b2e23d28f75c53fde830f255e45a62f21dbfe
SITA RAMI REDDY TATIKONDA <140719579+Seetu1234@users.noreply.github.com>
2023-08-30 10:46:24 +0530

    Add files via upload

---
+++ Body.js
@@ -0,0 +1,239 @@
+import React from "react";
+import "./Body.css";
+function Body() {
+  return (
+    <div
+      style={{
+        justifyContent: "center",
+        alignContent: "center",
+        width: "100vw",
+        fontWeight: "400",
+      }}
+    >
+      <div style={{ paddingLeft: "20%", paddingRight: "20%" }}>
+        <h2>Bus for Rental in Ahmedabad</h2>
+        <p>
+          Ahmedabad, the commercial hub of Gujarat, is one of the largest cities
+          in India. The sheer business nature of this place has attracted many
+          youngsters to begin their start up and become trendsetters in their
+          arena. Tycoons from various competitive fields add to the charm of the
+          ever-evolving city. But going beyond commercialism, the city has a lot
+          to offer, owing to its Mughal roots and colonial connections. With our
+          wide range of services in the city, no stone has been left unturned to
+          keep our customers satisfied. Getting buses for rent in Ahmedabad is
+          easier than ever with redBus.
+        </p>
+      </div>
+      <div style={{ backgroundColor: "#E5EBF8" }}>
+        <div style={{ paddingLeft: "20%", paddingRight: "20%" }}>
+          <h2>Hire Bus in Ahmedabad</h2>
+          <p>
+            In the past few years, Ahmedabad has seen a sharp rise in the count
+            of bus travellers. To cater to this increasing demand, more and more
+            tickets (and consequently, seats) are being made available. With one
+            of the largest road networks across the globe, it is only obvious
+            that Indians, especially the ones living in Ahmedabad, choose to
+            travel by the roads unconditionally. Besides, people choose to
+            travel from city to city by bus for its convenience and its economic
+            viability.
+          </p>
+          <p>
+            At redBus, we make it our top priority that your travel is
+            hassle-free, convenient, and most importantly, safe. Here is how the
+            booking is made simpler on redBus:
+          </p>
+          <ul>
+            <li>
+              You get the option to select the starting point and the
+              destination
+            </li>
+            <li>
+              You can choose one from many buses that are available in the
+              inventory for your journey
+            </li>
+            <li>You can also select the route according to your preference</li>
+          </ul>
+        </div>
+      </div>
+      <div style={{ paddingLeft: "20%", paddingRight: "20%" }}>
+        <h2>Advantages of Renting A Bus in Ahmedabad</h2>
+        <p>
+          Regardless of whether you are planning a business trip, a road trip,
+          or simply spending quality time with friends and family in Ahmedabad,
+          your choice of transport will play a key role during your tour. You
+          will be choosing the vehicle depending on various factors, including
+          terrain, and the number of people travelling. Let us take a look at
+          the benefits you will be getting when you rent/hire a bus in
+          Ahmedabad:
+        </p>
+        <ul>
+          <li>
+            Travelling by bus is economical compared to travelling by personal
+            cars. More the number of cars you have, more the number of vehicles
+            that need to be fueled. You will end up spending multiple times more
+            than what you will pay by using a 20 seater bus.
+          </li>
+          <li>
+            You will travel together, along with your close friends. You will
+            have time to catch up аnd tell stories on the bus as you trаvel in
+            an out of Ahmedаbаd.
+          </li>
+          <li>
+            Long-distance trаvel buses hаve inclined seats such thаt you can
+            slide and sleep if you want to rest. This is а big plus, as comfort
+            is а major determining fаctor аs you choose your vehicle of choice.
+          </li>
+          <li>
+            Moreover, using buses is better for the environment, as well as the
+            traffic density on the road, even when the roads in the city are
+            well laid out.
+          </li>
+        </ul>
+      </div>
+      <div style={{ paddingLeft: "20%", paddingRight: "20%" }}>
+        <h2>Types of Buses</h2>
+        <p>
+          To cater to the requirements of different travellers, redBus has made
+          available a wide range of buses for bus hire in Ahmedabad. These
+          include:
+        </p>
+        <ul>
+          <li>30 Seater</li>
+          <li>35 Seater</li>
+          <li>40 Seater</li>
+          <li>45 Seater</li>
+          <li>50 Seater</li>
+        </ul>
+      </div>
+      <div style={{ paddingLeft: "20%", paddingRight: "20%" }}>
+        <h2>Benefits of Online Bus Booking</h2>
+        <p>
+          There аre multiple benefits to online bus booking, some of which аre
+          listed below:
+        </p>
+        <ul>
+          <li>
+            The bus reservаtion fаcility is аvаilаble 24x7. You cаn book the
+            buses from anywhere, at any time
+          </li>
+          <li>
+            From the different bus operаtors functioning in Ahmedаbаd, you cаn
+            select one of your choices
+          </li>
+          <li>
+            With redBus, you don’t hаve to get аny trаvel аgent to rent а bus
+            for you. Our user-friendly interfаce promotes direct ticket booking
+            by the travellers
+          </li>
+          <li>
+            Moreover, with а simplified аnd cleаr method, the trаveller will be
+            trаvelling carefree
+          </li>
+          <li>
+            Also, you hаve the provision to choose your seаts, аnd will not hаve
+            to settle for something you don’t wаnt
+          </li>
+        </ul>
+      </div>
+      <div style={{ paddingLeft: "20%", paddingRight: "20%" }}>
+        <h2>Things to know about Bus Rental in Ahmedabad</h2>
+        <p>
+          Bus rentals in Ahmedabad are common for office events, award shows,
+          celebrity pickup, wedding functions, etc. One can easily rent a bus in
+          Ahmedabad via redBus mobile application/website. Renting a bus will
+          help you in staying away from the crowd amidst this COVID-19 scenario.
+          You can travel safely by bus on rent in Ahmedabad. One can also get a
+          driver for their rented bus in Ahmedabad via redBus. Many operators in
+          Ahmedabad provide a bus on rent like Dharti Travels, Sahjanand
+          Travels, Navbharat Tours, Ajitnath Travels Pvt. Ltd., etc.
+        </p>
+        <p>
+          You can choose buses on rent with the required amenities in Ahmedabad
+          on redBus. One can choose from different types of buses for rent in
+          Ahmedabad depending on seating capacity & manufacturer via redBus.
+          redBus also launched a Safety+ feature to tackle the challenges of the
+          recent COVID-19 outbreak. The safety measures followed on buses rented
+          via redBus are:
+        </p>
+        <ul>
+          <li>
+            Staff members wear masks & the buses are sanitised after each trip.
+          </li>
+          <li>Hand sanitisers will be provided to passengers on-board.</li>
+          <li>
+            Temperature checking facility will be provided to passengers from
+            the operator at the time of boarding.
+          </li>
+          <li>
+            Blankets/linens will not be provided by the operator due to the
+            COVID-19 scenario.
+          </li>
+        </ul>
+      </div>
+      <div style={{ paddingLeft: "20%", paddingRight: "20%" }}>
+        <h2>Here are a few tips to know before renting a bus in Ahmedabad.</h2>
+        <ul>
+          <li>
+            You should wear a mask while travelling and use hand sanitisers to
+            protect yourself from the COVID-19 outbreak.
+          </li>
+          <li>
+            Choose redBus as your booking platform to get the most affordable
+            services. redBus will also provide you with ride insurance & a bus
+            tracking facility. You can also get a driver for your bus on rent in
+            Ahmedabad. redBus will help you in comparing different operator
+            prices before renting a bus in Ahmedabad.
+          </li>
+          <li>
+            Winter is the best time to visit Ahmedabad as the temperature ranges
+            in the 30s. You can pack a sweater/overcoat with you for nights.
+          </li>
+        </ul>
+      </div>
+      <div style={{ backgroundColor: "#FFF5E5" }}>
+        <div style={{ paddingLeft: "20%", paddingRight: "20%" }}>
+          <h2>
+            You can hire a bus from redBus online<br></br>
+            following these steps:
+          </h2>
+          <ul>
+            <li>
+              {" "}
+              Go to the redBus portal and enter 'City of Hire,''Starting Point'
+              & 'Destination'
+            </li>
+            <li> Click search</li>
+            <li> redBus will bring the best operators at affordable prices</li>
+            <li>Select the bus operator and confirm the booking</li>
+          </ul>
+        </div>
+      </div>
+      <div style={{ paddingLeft: "20%", paddingRight: "20%" }}>
+        <h2>Benefits of Booking A Bus Online</h2>
+        <p>When you book online, the following benefits are offered:</p>
+        <ul>
+          <li>
+            <b>Inventory</b> - Access to leading vehicle providers in the city
+          </li>
+          <li>
+            <b>Zero booking fee</b> - Transparent booking. No hidden fee
+          </li>
+          <li>
+            <b>Choose vehicle </b>- Check vehicle images before making any
+            choice
+          </li>
+        </ul>
+      </div>
+      <div style={{ paddingLeft: "20%", paddingRight: "20%" }}>
+        <h2>Talk to Us</h2>
+        <p>
+          Drop your query at bushire@redbus.in to get all your questions
+          answered. We are glad to answer all your questions to ensure a smooth
+          booking process.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Body;
---
+++ Main.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Content from "./Content";
+import Drop from "./Drop";
+import Vechile from "./Vechile";
+import Body from "./Body";
+function Main() {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Content></Content>
+      <Drop></Drop>
+      <Vechile></Vechile>
+      <Body></Body>
+    </div>
+  );
+}
+
+export default Main;
